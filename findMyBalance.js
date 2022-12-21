386192629ca6362d85349743d95865108bee00a5
Payam Beigi <beigi.payam@gmail.com>
2022-12-20 23:40:42 -0800

    Retrieve balance using the signer abstraction and return its value

---
+++ findMyBalance.js
@@ -0,0 +1,10 @@
+const { Wallet, providers } = require('ethers');
+const { ganacheProvider } = require('./config');
+
+const provider = new providers.Web3Provider(ganacheProvider);
+
+function findMyBalance(privateKey) {
+    const wallet = new Wallet(privateKey, provider);
+    const balance = wallet.getBalance("latest");
+    return balance;
+}
