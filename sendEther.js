c5269213de471ce6b2f5a69f49668751f610fe3f
Payam Beigi <beigi.payam@gmail.com>
2022-12-20 23:25:44 -0800

    Use sendTransaction to automatically fill in nonce and sign transaction and broadcast it to the network

--- sendEther.js
+++ sendEther.js
@@ -4,14 +4,13 @@ const { ganacheProvider, PRIVATE_KEY } = require('./config');
 // Ganache provider for the testing purposes
 const provider = new providers.Web3Provider(ganacheProvider);
 
-const wallet = new Wallet(PRIVATE_KEY);
+const wallet = new Wallet(PRIVATE_KEY, provider);
 
 async function sendEther({ value, to }) {
-    const rawTx = await wallet.signTransaction({ 
+    const rawTx = await wallet.sendTransaction({ 
         value, to, 
         gasLimit: 0x5208,
         gasPrice: 0x3b9aca00 
     });
-
-    return provider.sendTransaction(rawTx);
+    return rawTx;
 }
